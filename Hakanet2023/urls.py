ad4b81e39dbeafe011d589e999f05e99d5706dde
Anikanov Vlad <anikanovvlad@vk.com>
2023-04-30 12:55:12 +0500

    some fixes

--- Hakanet2023/urls.py
+++ Hakanet2023/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
-from main.views import *
+from main.views import start_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),

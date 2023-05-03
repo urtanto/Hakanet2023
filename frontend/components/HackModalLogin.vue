798604f76df2489fd6b97487efba7b1e101a13cc
Васильев Александр <wyndace.av@ya.ru>
2023-05-03 12:54:17 +0500

    feat: начата работа над регистрацией

--- frontend/components/HackModalLogin.vue
+++ frontend/components/HackModalLogin.vue
@@ -58,13 +58,15 @@
       <div class="flex items-center">
         <input
           checked
-          id="checkbox-1"
+          id="checkboxlog"
+          name="checkboxlog"
           type="checkbox"
-          value="accepted"
+          :value="formData.accepted"
           class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
         />
         <label
-          for="checkbox-1"
+          for="checkboxlog"
+          name="checkboxlog"
           class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
           >Я согласен с
           <nuxt-link
@@ -94,6 +96,7 @@ export default defineComponent({
       formData: {
         login: "",
         password: "",
+        accepted: true,
       },
     }
   },

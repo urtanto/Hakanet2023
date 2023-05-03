ba3a70fa6cb450620dfab64ac676dcb9d2b4da90
franchesco_ <frank.artur2005@gmail.com>
2023-05-03 12:34:01 +0500

    feat(tailwind config): add colors

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -3,4 +3,10 @@ import type { Config } from "tailwindcss"
 export default <Partial<Config>>{
   plugins: [require("flowbite")],
   content: ["./node_modules/flowbite.{js,ts}"],
+  theme: {
+    colors: {
+      accent: "216146",
+      default: "eae9e7",
+    },
+  },
 }
